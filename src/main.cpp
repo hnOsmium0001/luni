da86325b95b4aee51edacf181ee111aa819fdc0e
hnOsmium0001 <36975818+hnOsmium0001@users.noreply.github.com>
2020-02-12 19:49:08 +0000

    Primitive command line argument parsing

---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+#include <stdexcept>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <array>
+#include <iostream>
+#include <utility>
+#include "aliases.hpp"
+
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+        return !std::isspace(ch);
+    }));
+}
+
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+static inline void trim(std::string &s) {
+    ltrim(s);
+    rtrim(s);
+}
+
+constexpr usize LOC_FILE_NAME = 0;
+constexpr usize LOC_DUMP_LOG = 1;
+constexpr usize NUM_LOC = 2;
+
+// TODO use tclap
+auto ParseArguments(
+    i32 argc,
+    char** argv
+) -> std::pair<std::array<std::string, NUM_LOC>, std::vector<std::string>> {
+    auto arguments = std::array<std::string, NUM_LOC>{};
+    auto errors = std::vector<std::string>{};
+
+    if (argc > 1) arguments[LOC_FILE_NAME] = argv[1];
+    else errors.push_back("No input file specified");
+
+    for (auto i = usize{2}; i < argc; ++i) {
+        auto arg = std::string{argv[i]};
+        if (arg == "-l" || arg == "--log") {
+            arguments[LOC_DUMP_LOG] = "--log";
+        }
+    }
+
+    return {arguments, errors};
+}
+
+i32 main(i32 argc, char** argv) {
+    auto [args, errors] = ParseArguments(argc, argv);
+    if (errors.size() > 0) {
+        std::cout << errors.size() << " errors generated: \n";
+        for (auto& msg : errors) std::cout << "\t" << msg << "\n";
+        std::cout << "Aborting.\n";
+        return -1;
+    }
+
+    std::cout << args[LOC_FILE_NAME] << "\n";
+    std::cout << args[LOC_DUMP_LOG] << "\n";
+    return 0;
+}
